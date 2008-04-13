b4899b955cf78f1408e06b9ce4bf3aa207d832f2
marcel_may <marcel_may@3e37aacf-aa1b-0410-bac3-f194596969e7>
2008-04-13 22:36:24 +0000

    Added experimental Spring xml namespace handler

---
+++ trunk/spring/src/main/java/com/icegreen/greenmail/spring/GreenMailBeanDefinitionParser.java
@@ -0,0 +1,47 @@
+package com.icegreen.greenmail.spring;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.util.xml.DomUtils;
+import org.w3c.dom.Element;
+
+/**
+ * Parses the GreenMail bean definition.
+ *
+ * @author Marcel May (mm)
+ */
+public class GreenMailBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+    /** The default hostname ({@value}). */
+    public static final String DEFAULT_HOSTNAME = "localhost";
+    /** The default port offset ({@value}). */
+    private static final Integer DEFAULT_PORT_OFFSET = Integer.valueOf(3000);
+
+    /** {@inheritDoc} */
+    @Override
+    protected Class getBeanClass(final Element element) {
+        return GreenMailBean.class;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doParse(final Element element, final BeanDefinitionBuilder builder) {
+        builder.addPropertyValue("hostname", extractHostname(element));
+        builder.addPropertyValue("portOffset", extractPortOffset(element));
+    }
+
+    private Object extractPortOffset(final Element pElement) {
+        Element portOffsetElement = DomUtils.getChildElementByTagName(pElement, "portOffset");
+        if(null!=portOffsetElement) {
+            return portOffsetElement.getTextContent();
+        }
+        return DEFAULT_PORT_OFFSET;
+    }
+
+    private Object extractHostname(final Element pElement) {
+        Element hostnameElement = DomUtils.getChildElementByTagName(pElement, "hostname");
+        if(null!=hostnameElement) {
+            return hostnameElement.getTextContent();
+        }
+        return DEFAULT_HOSTNAME;
+    }
+}
