fe5c118525999de2ed6a5ed3c82037e53a159f2c
Christian Amann <christian.amann@qaware.de>
2014-10-10 20:43:17 +0200

    Generify

--- greenmail-core/src/main/java/com/icegreen/greenmail/pop3/commands/Pop3CommandRegistry.java
+++ greenmail-core/src/main/java/com/icegreen/greenmail/pop3/commands/Pop3CommandRegistry.java
@@ -10,49 +10,27 @@ import java.util.Map;
 
 
 public class Pop3CommandRegistry {
-    private static Map commands = new HashMap();
-    private static Object[][] COMMANDS = new Object[][]
-    {
-        {"QUIT", QuitCommand.class}, {"STAT", StatCommand.class},
-        {"APOP", ApopCommand.class}, {"USER", UserCommand.class},
-        {"PASS", PassCommand.class}, {"LIST", ListCommand.class},
-        {"UIDL", UidlCommand.class}, {"TOP", TopCommand.class},
-        {"RETR", RetrCommand.class}, {"DELE", DeleCommand.class}
-        , {"NOOP", NoopCommand.class}, {"RSET", RsetCommand.class}
+    private final static Map<String, Pop3Command> commands = new HashMap<String, Pop3Command>();
 
-    };
-
-    public void load()
-            throws Exception {
-        for (int i = 0; i < COMMANDS.length; i++) {
-            String name = COMMANDS[i][0].toString();
-
-            if (commands.containsKey(name))
-
-                continue;
-
-            try {
-                Class type = (Class) COMMANDS[i][1];
-                Pop3Command command = (Pop3Command) type.newInstance();
-                registerCommand(name, command);
-            } catch (Exception e) {
-                throw e;
-            }
-        }
-    }
-
-    private void registerCommand(String name, Pop3Command command) {
-        commands.put(name, command);
+    public void load() {
+        commands.put("QUIT", new QuitCommand());
+        commands.put("STAT", new StatCommand());
+        commands.put("APOP", new ApopCommand());
+        commands.put("USER", new UserCommand());
+        commands.put("PASS", new PassCommand());
+        commands.put("LIST", new ListCommand());
+        commands.put("UIDL", new UidlCommand());
+        commands.put("TOP", new TopCommand());
+        commands.put("RETR", new RetrCommand());
+        commands.put("DELE", new DeleCommand());
+        commands.put("NOOP", new NoopCommand());
+        commands.put("RSET", new RsetCommand());
     }
 
     public Pop3Command getCommand(String name) {
         if (commands.size() == 0) {
-            try {
-                load();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            load();
         }
-        return (Pop3Command) commands.get(name);
+        return commands.get(name);
     }
 }
