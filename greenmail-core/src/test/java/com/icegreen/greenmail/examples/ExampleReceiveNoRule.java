844b85f26af07efcc5de6c729f5ea75c0e3f5c38
Christian Amann <christian.amann@qaware.de>
2015-01-08 09:54:26 +0100

    #45 Put JUnit rule tests on top of the website

---
+++ greenmail-core/src/test/java/com/icegreen/greenmail/examples/ExampleReceiveNoRule.java
@@ -0,0 +1,37 @@
+package com.icegreen.greenmail.examples;
+
+import com.icegreen.greenmail.user.GreenMailUser;
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.Test;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleReceiveNoRule {
+    @Test
+    public void testReceive() throws MessagingException, IOException {
+        //Start all email servers using non-default ports.
+        GreenMail greenMail = new GreenMail(ServerSetupTest.ALL);
+        greenMail.start();
+
+        //Use random content to avoid potential residual lingering problems
+        final String subject = GreenMailUtil.random();
+        final String body = GreenMailUtil.random();
+        MimeMessage message = createMimeMessage(subject, body, greenMail); // Construct message
+        GreenMailUser user = greenMail.setUser("wael@localhost.com", "waelc", "soooosecret");
+        user.deliver(message);
+        assertEquals(1, greenMail.getReceivedMessages().length);
+
+        // --- Place your retrieve code here
+        greenMail.stop();
+    }
+
+    private MimeMessage createMimeMessage(String subject, String body, GreenMail greenMail) {
+        return GreenMailUtil.createTextEmail("to@localhost.com", "from@localhost.com", subject, body, greenMail.getImap().getServerSetup());
+    }
+}
---
+++ greenmail-core/src/test/java/com/icegreen/greenmail/examples/ExampleRule.java
@@ -0,0 +1,27 @@
+package com.icegreen.greenmail.examples;
+
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.Rule;
+import org.junit.Test;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleRule {
+    @Rule
+    public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.ALL);
+
+    @Test
+    public void testSomething() throws MessagingException {
+        GreenMailUtil.sendTextEmailTest("to@localhost.com", "from@localhost.com", "subject", "body");
+        MimeMessage[] emails = greenMail.getReceivedMessages();
+        assertEquals(1, emails.length);
+        assertEquals("subject", emails[0].getSubject());
+        assertEquals("body", GreenMailUtil.getBody(emails[0]));
+        // ...
+    }
+}
