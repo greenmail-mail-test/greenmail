7be8819dbc091eb8fb9d22f036b915f23922dec1
Raimund Klein <raimund.klein@gmx.de>
2015-06-04 22:29:22 +0100

    Simple refactoring in order to ease exchanging the StoredMailCollection implementation.

--- greenmail-core/src/main/java/com/icegreen/greenmail/imap/ImapHostManagerImpl.java
+++ greenmail-core/src/main/java/com/icegreen/greenmail/imap/ImapHostManagerImpl.java
@@ -28,8 +28,7 @@ public class ImapHostManagerImpl
      * Hack constructor which creates an in-memory store, and creates a console logger.
      */
     public ImapHostManagerImpl() {
-        store = new InMemoryStore();
-        subscriptions = new MailboxSubscriptions();
+        this(new InMemoryStore());
     }
 
     public ImapHostManagerImpl(Store store) {
@@ -284,6 +283,10 @@ public class ImapHostManagerImpl
         }
     }
 
+    public Store getStore() {
+        return store;
+    }
+
     /**
      * Handles all user subscriptions.
      * TODO make this a proper class
@@ -337,9 +340,5 @@ public class ImapHostManagerImpl
             }
             return subs;
         }
-    }
-
-    public Store getStore() {
-        return store;
     }
 }
