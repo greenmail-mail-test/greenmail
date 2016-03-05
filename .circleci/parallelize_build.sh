8c97bac06a1341cabb06dc47b64ef5f5916335bd
Marcel May <marcel.may.de@gmail.com>
2016-03-05 12:26:03 +0100

    Issue #118 : Require minimum JDK 1.7

--- .circleci/parallelize_build.sh
+++ .circleci/parallelize_build.sh
@@ -12,11 +12,6 @@ run_maven() {
  mvn test
 }
 
-jdk6() {
- sudo update-alternatives --set java "/usr/lib/jvm/jdk1.6.0/bin/java"
- sudo update-alternatives --set javac "/usr/lib/jvm/jdk1.6.0/bin/javac"
-}
-
 jdk7() {
  sudo update-alternatives --set java "/usr/lib/jvm/jdk1.7.0/bin/java"
  sudo update-alternatives --set javac "/usr/lib/jvm/jdk1.7.0/bin/javac"
@@ -27,47 +22,41 @@ jdk8() {
  sudo update-alternatives --set javac "/usr/lib/jvm/jdk1.8.0/bin/javac"
 }
 
-openjdk6() {
- sudo update-alternatives --set java "/usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java"
- sudo update-alternatives --set javac "/usr/lib/jvm/java-6-openjdk-amd64/bin/javac"
-}
-
 openjdk7() {
  sudo update-alternatives --set java "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java"
  sudo update-alternatives --set javac "/usr/lib/jvm/java-7-openjdk-amd64/bin/javac"
 }
 
+openjdk8() {
+ sudo update-alternatives --set java "/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
+ sudo update-alternatives --set javac "/usr/lib/jvm/java-8-openjdk-amd64/bin/javac"
+}
 case $CIRCLE_NODE_INDEX in 
 	0)
-	 echo "Building GreenMail on Oracle JDK6"
-	 jdk6
-	 run_maven
-	 copy_artifacts "jdk1.6.0"
-	 ;;
-	
-	1)
 	 echo "Building GreenMail on Oracle JDK7"
 	 jdk7
 	 run_maven
 	 copy_artifacts "jdk1.7.0"
 	 ;;
 	
-	2)
+	1)
 	 echo "Building GreenMail on Oracle JDK8"
 	 jdk8
-         run_maven
+     run_maven
 	 copy_artifacts "jdk1.8.0"
 	 ;;
 
-	3)
-	 echo "Building GreenMail on OpenJDK6"
-	 openjdk6
-	 run_maven
-	 copy_artifacts "java-6-openjdk-amd64" 
+	2)
 	 echo "Building GreenMail on OpenJDK7"
 	 openjdk7
-         run_maven
+     run_maven
 	 copy_artifacts "java-7-openjdk-amd64"
+	 ;;
+	3)
+	 echo "Building GreenMail on OpenJDK8"
+	 openjdk8
+     run_maven
+	 copy_artifacts "java-8-openjdk-amd64"
 	 ;;
 esac
 
