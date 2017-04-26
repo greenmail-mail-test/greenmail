greenmail-mail-test/greenmail

commit 47af7fc258e00de9c80bdd6615a8ecf8965253e4

Issue #184 : Support search by TEXT
- Added CHARSET support and test

greenmail-core/src/main/java/com/icegreen/greenmail/imap/commands/SearchCommandParser.java
@@ -14,6 +14,9 @@ import org.slf4j.LoggerFactory;
 import javax.mail.search.AndTerm;
 import javax.mail.search.NotTerm;
 import javax.mail.search.SearchTerm;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
 
 import static com.icegreen.greenmail.imap.commands.IdRange.SEQUENCE;
 
@@ -24,6 +27,7 @@ import static com.icegreen.greenmail.imap.commands.IdRange.SEQUENCE;
  */
 class SearchCommandParser extends CommandParser {
     private final Logger log = LoggerFactory.getLogger(SearchCommandParser.class);
+    private static final String CHARSET_TOKEN = "CHARSET";
 
     /**
      * Parses the request argument into a valid search term. Not yet fully implemented - see SearchKey enum.
@@ -31,7 +35,7 @@ class SearchCommandParser extends CommandParser {
      * Other searches will return everything for now.
      */
     public SearchTerm searchTerm(ImapRequestLineReader request)
-            throws ProtocolException {
+            throws ProtocolException, CharacterCodingException {
         SearchTerm resultTerm = null;
         SearchTermBuilder b = null;
         boolean negated = false;
@@ -40,19 +44,21 @@ class SearchCommandParser extends CommandParser {
         char next = request.nextChar();
         StringBuilder sb = new StringBuilder();
         boolean quoted = false;
+        Charset charset = null;
+
         while (next != '\n') {
-            if (next != '\"' && (quoted || (next != '\"' && next != 32 /* space */ && next != 13 /* cr */ ))) {
+            if (next != '\"' && (quoted || (next != '\"' && next != CHR_SPACE && next != CHR_CR))) {
                 sb.append(next);
             }
             request.consume();
             next = request.nextChar();
-            if(next == '\"') {
+            if (next == '\"') {
                 quoted = !quoted;
-                if(quoted) {
+                if (quoted) {
                     continue;
                 }
             }
-            if (!quoted && (next == 32 || next == '\n') && sb.length()>0) {
+            if (!quoted && (next == CHR_SPACE || next == '\n') && sb.length() > 0) {
                 if (log.isDebugEnabled()) {
                     log.debug("Search request is '" + sb.toString() + '\'');
                 }
@@ -76,12 +82,20 @@ class SearchCommandParser extends CommandParser {
                             // Sequence can be a whitespace separated list of either a number or number range
                             // Example: '2 5:9 9'
                             next = request.nextChar();
-                            while(next == 32 || (next >= '0' && next <= '9') || next == ':') {
+                            while (next == CHR_SPACE || Character.isDigit(next) || next == ':') {
                                 request.consume();
                                 sb.append(next);
                                 next = request.nextChar();
                             }
                             b.addParameter(sb.toString());
+                        }
+                        else if (CHARSET_TOKEN.equals(keyValue)) { // Charset handling
+                            next = request.nextWordChar();
+                            String c = this.atom(request);
+                            if (log.isDebugEnabled()) {
+                                log.debug("Searching with given CHARSET <" + c + '>');
+                            }
+                            charset = Charset.forName(c);
                         } else {
                             // Term?
                             SearchKey key = SearchKey.valueOf(keyValue);
@@ -90,6 +104,17 @@ class SearchCommandParser extends CommandParser {
                             } else {
                                 b = SearchTermBuilder.create(key);
                             }
+
+                            if (b.expectsParameter() && key.isCharsetAware() && null != charset && next == CHR_SPACE) {
+                                next = request.nextWordChar();
+                                if (next == '{') {
+                                    String textOfCharset = new String(consumeLiteralAsBytes(request), charset);
+                                    b.addParameter(textOfCharset);
+                                    if(log.isDebugEnabled()) {
+                                        log.debug("Searching for text <"+textOfCharset+"> of charset "+charset);
+                                    }
+                                }
+                            }
                         }
                     } catch (IllegalArgumentException ex) {
                         // Ignore for now instead of breaking. See issue#35 .
@@ -97,7 +122,23 @@ class SearchCommandParser extends CommandParser {
                         negated = false;
                     }
                 } else if (b.expectsParameter()) {
-                    b = b.addParameter(sb.toString());
+                    if (b.isCharsetAware() && null != charset) {
+                        request.consume(); // \n
+                        next = request.nextChar();
+                        final Integer capacity = Integer.valueOf(sb.substring(1, sb.length() - 1));
+                        ByteBuffer bb = ByteBuffer.allocate(capacity);
+                        while (next != CHR_CR) {
+                            request.consume(); // \n
+                            //   bb.putChar(next);
+                            sb.append(next);
+                            next = request.nextChar();
+                        }
+                        final String decoded = charset.decode(bb).toString();
+                        log.info("Decoded <" + bb + "> into <" + decoded + ">");
+                        b = b.addParameter(decoded);
+                    } else {
+                        b = b.addParameter(sb.toString());
+                    }
                 }
 
                 if (b != null && !b.expectsParameter()) {
@@ -116,5 +157,4 @@ class SearchCommandParser extends CommandParser {
 
         return resultTerm;
     }
-
 }

greenmail-core/src/test/java/com/icegreen/greenmail/imap/commands/ImapProtocolTest.java
@@ -2,9 +2,9 @@ package com.icegreen.greenmail.imap.commands;
 
 import com.icegreen.greenmail.junit.GreenMailRule;
 import com.icegreen.greenmail.util.GreenMailUtil;
-import com.icegreen.greenmail.util.ServerSetup;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import com.sun.mail.iap.ByteArray;
+import com.sun.mail.iap.Argument;
 import com.sun.mail.iap.ProtocolException;
 import com.sun.mail.iap.Response;
 import com.sun.mail.imap.IMAPFolder;
@@ -18,6 +18,7 @@ import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,7 +40,8 @@ public class ImapProtocolTest {
         int numberOfMails = 10;
         for (int i = 0; i < numberOfMails; i++) {
             GreenMailUtil.sendTextEmail("foo@localhost", "bar@localhost", "Test search " + i,
-                    "Test message content" + i, ServerSetupTest.SMTP);
+                    "Test message content" + i,
+                    greenMail.getSmtp().getServerSetup());
         }
         greenMail.waitForIncomingEmail(numberOfMails);
 
@@ -273,7 +275,6 @@ public class ImapProtocolTest {
     }
 
     @Test
-<<<<<<< HEAD
     public void testUidSearchText() throws MessagingException, IOException {
         store.connect("foo@localhost", "pwd");
         try {
@@ -315,6 +316,7 @@ public class ImapProtocolTest {
         }
     }
 
+    @Test
     public void testRenameFolder() throws MessagingException {
         store.connect("foo@localhost", "pwd");
         try {
@@ -343,6 +345,51 @@ public class ImapProtocolTest {
             final Folder bar = store.getFolder("bar");
             bar.open(Folder.READ_ONLY);
             assertTrue(bar.exists());
+        } finally {
+            store.close();
+        }
+    }
+
+    @Test
+    public void testUidSearchTextWithCharset() throws MessagingException, IOException {
+        greenMail.setUser("foo2@localhost", "pwd");
+        store.connect("foo2@localhost", "pwd");
+        try {
+            IMAPFolder folder = (IMAPFolder) store.getFolder("INBOX");
+            folder.open(Folder.READ_ONLY);
+
+            final MimeMessage email = GreenMailUtil.createTextEmail("foo2@localhost", "foo@localhost",
+                    "some subject", "some content",
+                    greenMail.getSmtp().getServerSetup());
+
+            String[][] s = {
+                    {"US-ASCII", "ABC", "1"},
+                    {"ISO-8859-15", "\u00c4\u00e4\u20AC", "2"},
+                    {"UTF-8", "\u00c4\u00e4\u03A0", "3"}
+            };
+
+            for (String[] charsetAndQuery : s) {
+                final String charset = charsetAndQuery[0];
+                final String search = charsetAndQuery[1];
+
+                email.setSubject("subject " + search, charset);
+                GreenMailUtil.sendMimeMessage(email);
+
+                // messages[2] contains content with search text, match must be case insensitive
+                final byte[] searchBytes = search.getBytes(charset);
+                final Argument arg = new Argument();
+                arg.writeBytes(searchBytes);
+                Response[] ret = (Response[]) folder.doCommand(new IMAPFolder.ProtocolCommand() {
+                    @Override
+                    public Object doCommand(IMAPProtocol protocol) throws ProtocolException {
+                        return protocol.command("UID SEARCH CHARSET " + charset + " TEXT", arg);
+                    }
+                });
+                IMAPResponse response = (IMAPResponse) ret[0];
+                assertFalse(response.isBAD());
+                String number = response.getRest();
+                assertEquals(charsetAndQuery[2], number);
+            }
         } finally {
             store.close();
         }

greenmail-core/src/main/java/com/icegreen/greenmail/imap/ImapRequestLineReader.java
@@ -142,9 +142,9 @@ public class ImapRequestLineReader {
 
     /**
      * Reads and consumes a number of characters from the underlying reader,
-     * filling the char array provided.
+     * filling the byte array provided.
      *
-     * @param holder A char array which will be filled with chars read from the underlying reader.
+     * @param holder A byte array which will be filled with bytes read from the underlying reader.
      * @throws ProtocolException If a char can't be read into each array element.
      */
     public void read(byte[] holder) throws ProtocolException {

greenmail-core/src/main/java/com/icegreen/greenmail/imap/commands/CommandParser.java
@@ -26,6 +26,15 @@ import java.util.Locale;
  */
 public class CommandParser {
     /**
+     * SPACE character
+     */
+    static final char CHR_SPACE = ' ';
+    /**
+     * Carriage-Return '\r' character
+     */
+    static final char CHR_CR = '\r';
+
+    /**
      * Reads an argument of type "atom" from the request.
      */
     public String atom(ImapRequestLineReader request) throws ProtocolException {
@@ -439,14 +448,10 @@ public class CommandParser {
     private static class MessageSetCharValidator implements CharacterValidator {
         @Override
         public boolean isValid(char chr) {
-            return isDigit(chr) ||
+            return Character.isDigit(chr) ||
                     chr == ':' ||
                     chr == '*' ||
                     chr == ',';
-        }
-
-        private boolean isDigit(char chr) {
-            return '0' <= chr && chr <= '9';
         }
     }
 

greenmail-core/src/main/java/com/icegreen/greenmail/imap/commands/SearchCommand.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.mail.search.SearchTerm;
+import java.nio.charset.CharacterCodingException;
 
 /**
  * Handles processing for the SEARCH imap command.
@@ -54,6 +55,10 @@ class SearchCommand extends SelectedStateCommand implements UidEnabledCommand {
         } catch(IllegalArgumentException ex) {
             // Not support => return "BAD"
             response.badResponse("Search command not supported");
+            return;
+        } catch (CharacterCodingException e) {
+            // Not support => return "BAD"
+            response.badResponse("Search command does not support charset "+e.getMessage());
             return;
         }
 

greenmail-core/src/main/java/com/icegreen/greenmail/imap/commands/SearchKey.java
@@ -68,22 +68,22 @@ package com.icegreen.greenmail.imap.commands;
 public enum SearchKey {
     ALL(),
     ANSWERED(),
-    BCC(1),
-    CC(1),
+    BCC(1, true),
+    CC(1, true),
     DELETED(),
     DRAFT(),
     FLAGGED(),
-    FROM(1),
-    HEADER(2),
+    FROM(1, true),
+    HEADER(2, true),
     KEYWORD(1),
     NEW(),
     NOT(true),
     OLD(),
     RECENT(),
     SEEN(),
-    SUBJECT(1),
-    TO(1),
-    TEXT(1),
+    SUBJECT(1, true),
+    TO(1, true),
+    TEXT(1, true),
     UID(1),
     UNANSWERED(),
     UNDELETED(),
@@ -99,6 +99,7 @@ public enum SearchKey {
 
     private int minArgs = 0; // expected additional arguments
     private boolean operator = false; // Is an operator, such as AND, OR, NOT ...
+    private boolean charsetAware = false;
 
     SearchKey() {
         // Nothing
@@ -106,6 +107,11 @@ public enum SearchKey {
 
     SearchKey(int pMinArgs) {
         minArgs = pMinArgs;
+    }
+
+    SearchKey(int pMinArgs, boolean charsetAware) {
+        this(pMinArgs);
+        this.charsetAware = charsetAware;
     }
 
     SearchKey(boolean pOperator) {
@@ -118,5 +124,9 @@ public enum SearchKey {
 
     public boolean isOperator() {
         return operator;
+    }
+
+    public boolean isCharsetAware() {
+        return charsetAware;
     }
 }

greenmail-core/src/main/java/com/icegreen/greenmail/imap/commands/SearchTermBuilder.java
@@ -147,6 +147,10 @@ public abstract class SearchTermBuilder {
         return parameters.size() < key.getNumberOfParameters();
     }
 
+    boolean isCharsetAware() {
+        return key.isCharsetAware();
+    }
+
     public abstract SearchTerm build();
 
     private static SearchTermBuilder createSearchTermBuilder(final SearchTerm pSearchTerm) {

greenmail-core/src/main/java/com/icegreen/greenmail/imap/commands/SortCommand.java
@@ -5,6 +5,7 @@ import com.icegreen.greenmail.store.FolderException;
 import com.icegreen.greenmail.store.MailFolder;
 import com.icegreen.greenmail.store.StoredMessage;
 
+import java.nio.charset.CharacterCodingException;
 import java.util.*;
 
 /**
@@ -38,7 +39,14 @@ class SortCommand extends SelectedStateCommand implements UidEnabledCommand {
                           ImapResponse response,
                           ImapSession session,
                           boolean useUids) throws ProtocolException, FolderException {
-        final SortTerm sortTerm = sortCommandParser.sortTerm(request);
+        final SortTerm sortTerm;
+        try {
+            sortTerm = sortCommandParser.sortTerm(request);
+        } catch (CharacterCodingException e) {
+            // Not support => return "BAD"
+            response.badResponse("Sort/search command does not support charset "+e.getMessage());
+            return;
+        }
 
         final MailFolder folder = session.getSelected();
 

commit 01c2b3d65ca34721694bed3932f89b1a7b836ba4

Fixes #201 : NPE on "SEARCH NOT ..."
Regression from 47af7fc

greenmail-core/src/main/java/com/icegreen/greenmail/imap/commands/SearchCommandParser.java
@@ -105,7 +105,7 @@ class SearchCommandParser extends CommandParser {
                                 b = SearchTermBuilder.create(key);
                             }
 
-                            if (b.expectsParameter() && key.isCharsetAware() && null != charset && next == CHR_SPACE) {
+                            if (null!=b && b.expectsParameter() && key.isCharsetAware() && null != charset && next == CHR_SPACE) {
                                 next = request.nextWordChar();
                                 if (next == '{') {
                                     String textOfCharset = new String(consumeLiteralAsBytes(request), charset);

greenmail-core/src/test/java/com/icegreen/greenmail/imap/commands/ImapProtocolTest.java
@@ -278,11 +278,33 @@ public class ImapProtocolTest {
     }
 
     @Test
+    public void testSearchNotFlags() throws MessagingException {
+        store.connect("foo@localhost", "pwd");
+        try {
+            IMAPFolder folder = (IMAPFolder) store.getFolder("INBOX");
+            folder.open(Folder.READ_WRITE);
+            folder.setFlags(new int[]{2,3}, new Flags(Flags.Flag.ANSWERED), true);
+            Response[] ret = (Response[]) folder.doCommand(new IMAPFolder.ProtocolCommand() {
+                @Override
+                public Object doCommand(IMAPProtocol protocol) throws ProtocolException {
+                    return protocol.command("SEARCH NOT (ANSWERED) NOT (DELETED) NOT (SEEN) NOT (FLAGGED) ALL", null);
+                }
+            });
+            IMAPResponse response = (IMAPResponse) ret[0];
+            assertFalse(response.isBAD());
+            assertEquals("1 4 5 6 7 8 9 10" /* 2 and 3 set to answered */, response.getRest());
+        } finally {
+            store.close();
+        }
+    }
+
+
+    @Test
     public void testGetMessageByUnknownUidInEmptyINBOX() throws MessagingException, FolderException {
         greenMail.getManagers()
-            .getImapHostManager()
-            .getInbox(user)
-            .deleteAllMessages();
+                .getImapHostManager()
+                .getInbox(user)
+                .deleteAllMessages();
         store.connect("foo@localhost", "pwd");
         try {
             IMAPFolder folder = (IMAPFolder) store.getFolder("INBOX");