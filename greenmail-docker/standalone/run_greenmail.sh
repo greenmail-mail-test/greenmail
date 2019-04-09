327f8420aaee5a3f2eb5ca03acadc269dea97da8
Jacob Middag <jacob@gaddim.nl>
2019-04-09 11:19:15 +0200

    Use exec to replace bash process with java process

--- greenmail-docker/standalone/run_greenmail.sh
+++ greenmail-docker/standalone/run_greenmail.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 echo "Executing 'java $JAVA_OPTS $GREENMAIL_OPTS -jar greenmail-standalone.jar' ..."
-java $JAVA_OPTS $GREENMAIL_OPTS -jar greenmail-standalone.jar
+exec java $JAVA_OPTS $GREENMAIL_OPTS -jar greenmail-standalone.jar
