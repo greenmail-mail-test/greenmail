ed4d89c50539ac9fae14ca62308f251f0eb834fa
Marcel May <marcel.may.de@gmail.com>
2020-03-08 16:16:55 +0100

    Retriever.close can throw NPE on close (fixes #299)
    Also merged c.i.g.test.RetrieverTest into c.i.g.util.RetrieverTest

--- greenmail-core/src/test/java/com/icegreen/greenmail/util/RetrieverTest.java
+++ greenmail-core/src/test/java/com/icegreen/greenmail/util/RetrieverTest.java
@@ -1,8 +1,40 @@
 package com.icegreen.greenmail.util;
 
+import com.icegreen.greenmail.junit.GreenMailRule;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class RetrieverTest {
+    @Rule
+    public final GreenMailRule greenMail = new GreenMailRule(new ServerSetup[]{
+            ServerSetupTest.IMAP,
+            ServerSetupTest.IMAPS,
+            ServerSetupTest.SMTP,
+            ServerSetupTest.SMTPS,
+            ServerSetupTest.POP3,
+            ServerSetupTest.POP3S
+    });
+
+    @Test
+    public void testCreateRetriever() {
+        try(Retriever r = new Retriever(greenMail.getImap())) {}
+        try(Retriever r = new Retriever(greenMail.getImaps())) {}
+        try(Retriever r = new Retriever(greenMail.getPop3())) {}
+        try(Retriever r = new Retriever(greenMail.getPop3s())) {}
+        try(Retriever r = new Retriever(greenMail.getSmtp())) {
+            fail("Expected IAE");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+        try(Retriever r = new Retriever(greenMail.getSmtps())) {
+            fail("Expected IAE");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorWithNullArg() {
         new Retriever(null);
