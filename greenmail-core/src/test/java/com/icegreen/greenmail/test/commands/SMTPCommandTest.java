7d177fd064807abef0b684eefee2239b17686cfd
Marcel May <marcel.may.de@gmail.com>
2020-04-01 21:29:20 +0200

    Minor test refactoring

--- greenmail-core/src/test/java/com/icegreen/greenmail/test/commands/SMTPCommandTest.java
+++ greenmail-core/src/test/java/com/icegreen/greenmail/test/commands/SMTPCommandTest.java
@@ -13,6 +13,7 @@ import com.icegreen.greenmail.smtp.commands.AuthCommand;
 import com.icegreen.greenmail.user.UserException;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import com.sun.mail.smtp.SMTPTransport;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -27,18 +28,24 @@ public class SMTPCommandTest {
     @Rule
     public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP);
 
+    private int port;
+    private String hostAddress;
+    private URLName smtpURL;
+
+    @Before
+    public void setUp() {
+        hostAddress = greenMail.getSmtp().getBindTo();
+        port = greenMail.getSmtp().getPort();
+        smtpURL = new URLName(hostAddress);
+    }
+
     @Test
     public void mailSenderEmpty() throws IOException, MessagingException {
-        Socket smtpSocket;
-        String hostAddress = greenMail.getSmtp().getBindTo();
-        int port = greenMail.getSmtp().getPort();
-
         Session smtpSession = greenMail.getSmtp().createSession();
-        URLName smtpURL = new URLName(hostAddress);
         SMTPTransport smtpTransport = new SMTPTransport(smtpSession, smtpURL);
 
         try {
-            smtpSocket = new Socket(hostAddress, port);
+            Socket smtpSocket = new Socket(hostAddress, port); // Closed by transport
             smtpTransport.connect(smtpSocket);
             assertThat(smtpTransport.isConnected(), is(equalTo(true)));
             smtpTransport.issueCommand("MAIL FROM: <>", -1);
@@ -49,83 +56,41 @@ public class SMTPCommandTest {
     }
 
     @Test
-        public void authPlain() throws IOException, MessagingException, UserException {
-            Socket smtpSocket;
-            String hostAddress = greenMail.getSmtp().getBindTo();
-            int port = greenMail.getSmtp().getPort();
-
-            URLName smtpURL = new URLName(hostAddress);
-            {
-                Session smtpSession = greenMail.getSmtp().createSession();
-                SMTPTransport smtpTransport = new SMTPTransport(smtpSession, smtpURL);
-                try {
-                    smtpSocket = new Socket(hostAddress, port);
-                    smtpTransport.connect(smtpSocket);
-                    assertThat(smtpTransport.isConnected(), is(equalTo(true)));
-
-                    // Should fail, as user does not exist
-                    smtpTransport.issueCommand("AUTH PLAIN dGVzdAB0ZXN0AHRlc3RwYXNz" /* test / test / testpass */, -1);
-                    assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_CREDENTIALS_INVALID));
-
-                    // Try again but create user
-                    greenMail.getManagers().getUserManager().createUser("test@localhost", "test", "testpass");
-                    smtpTransport.issueCommand("AUTH PLAIN dGVzdAB0ZXN0AHRlc3RwYXNz" /* test / test / testpass */, -1);
-                    assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_SUCCEDED));
-                } finally {
-                    smtpTransport.close();
-                }
-            }
-
-            // With continuation
-            {
-                Session smtpSession = greenMail.getSmtp().createSession();
-                SMTPTransport smtpTransport = new SMTPTransport(smtpSession, smtpURL);
-                try {
-                    smtpSocket = new Socket(hostAddress, port);
-                    smtpTransport.connect(smtpSocket);
-                    assertThat(smtpTransport.isConnected(), is(equalTo(true)));
-
-                    smtpTransport.issueCommand("AUTH PLAIN", -1);
-                    assertTrue(smtpTransport.getLastServerResponse().startsWith(AuthCommand.SMTP_SERVER_CONTINUATION));
-                    smtpTransport.issueCommand("dGVzdAB0ZXN0AHRlc3RwYXNz" /* test / test / testpass */, -1);
-                    assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_SUCCEDED));
-                } finally {
-                    smtpTransport.close();
-                }
-            }
-        }
-
-    @Test
-    public void authLogin() throws IOException, MessagingException, UserException {
-        Socket smtpSocket;
-        String hostAddress = greenMail.getSmtp().getBindTo();
-        int port = greenMail.getSmtp().getPort();
-
-        URLName smtpURL = new URLName(hostAddress);
+    public void authPlain() throws IOException, MessagingException, UserException {
         {
             Session smtpSession = greenMail.getSmtp().createSession();
             SMTPTransport smtpTransport = new SMTPTransport(smtpSession, smtpURL);
             try {
-                smtpSocket = new Socket(hostAddress, port);
+                Socket smtpSocket = new Socket(hostAddress, port); // Closed by transport
                 smtpTransport.connect(smtpSocket);
                 assertThat(smtpTransport.isConnected(), is(equalTo(true)));
 
                 // Should fail, as user does not exist
-                smtpTransport.issueCommand("AUTH LOGIN ", 334);
-                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 VXNlciBOYW1lAA==" /* Username */));
-                smtpTransport.issueCommand(Base64.getEncoder().encodeToString("test".getBytes(StandardCharsets.US_ASCII)), -1);
-                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 UGFzc3dvcmQA" /* Password */));
-                smtpTransport.issueCommand(Base64.getEncoder().encodeToString("testpass".getBytes(StandardCharsets.US_ASCII)), -1);
-                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_CREDENTIALS_INVALID ));
+                smtpTransport.issueCommand("AUTH PLAIN dGVzdAB0ZXN0AHRlc3RwYXNz" /* test / test / testpass */, -1);
+                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_CREDENTIALS_INVALID));
 
                 // Try again but create user
                 greenMail.getManagers().getUserManager().createUser("test@localhost", "test", "testpass");
-                smtpTransport.issueCommand("AUTH LOGIN ", 334);
-                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 VXNlciBOYW1lAA==" /* Username */));
-                smtpTransport.issueCommand(Base64.getEncoder().encodeToString("test".getBytes(StandardCharsets.US_ASCII)), -1);
-                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 UGFzc3dvcmQA" /* Password */));
-                smtpTransport.issueCommand(Base64.getEncoder().encodeToString("testpass".getBytes(StandardCharsets.US_ASCII)), -1);
-                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_SUCCEDED ));
+                smtpTransport.issueCommand("AUTH PLAIN dGVzdAB0ZXN0AHRlc3RwYXNz" /* test / test / testpass */, -1);
+                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_SUCCEDED));
+            } finally {
+                smtpTransport.close();
+            }
+        }
+
+        // With continuation
+        {
+            Session smtpSession = greenMail.getSmtp().createSession();
+            SMTPTransport smtpTransport = new SMTPTransport(smtpSession, smtpURL);
+            try {
+                Socket smtpSocket = new Socket(hostAddress, port); // Closed by transport
+                smtpTransport.connect(smtpSocket);
+                assertThat(smtpTransport.isConnected(), is(equalTo(true)));
+
+                smtpTransport.issueCommand("AUTH PLAIN", -1);
+                assertTrue(smtpTransport.getLastServerResponse().startsWith(AuthCommand.SMTP_SERVER_CONTINUATION));
+                smtpTransport.issueCommand("dGVzdAB0ZXN0AHRlc3RwYXNz" /* test / test / testpass */, -1);
+                assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_SUCCEDED));
             } finally {
                 smtpTransport.close();
             }
@@ -133,17 +98,42 @@ public class SMTPCommandTest {
     }
 
     @Test
-    public void mailSenderAUTHSuffix() throws IOException, MessagingException {
-        Socket smtpSocket;
-        String hostAddress = greenMail.getSmtp().getBindTo();
-        int port = greenMail.getSmtp().getPort();
-
+    public void authLogin() throws IOException, MessagingException, UserException {
         Session smtpSession = greenMail.getSmtp().createSession();
-        URLName smtpURL = new URLName(hostAddress);
+        SMTPTransport smtpTransport = new SMTPTransport(smtpSession, smtpURL);
+        try {
+            Socket smtpSocket = new Socket(hostAddress, port); // Closed by transport
+            smtpTransport.connect(smtpSocket);
+            assertThat(smtpTransport.isConnected(), is(equalTo(true)));
+
+            // Should fail, as user does not exist
+            smtpTransport.issueCommand("AUTH LOGIN ", 334);
+            assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 VXNlciBOYW1lAA==" /* Username */));
+            smtpTransport.issueCommand(Base64.getEncoder().encodeToString("test".getBytes(StandardCharsets.US_ASCII)), -1);
+            assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 UGFzc3dvcmQA" /* Password */));
+            smtpTransport.issueCommand(Base64.getEncoder().encodeToString("testpass".getBytes(StandardCharsets.US_ASCII)), -1);
+            assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_CREDENTIALS_INVALID));
+
+            // Try again but create user
+            greenMail.getManagers().getUserManager().createUser("test@localhost", "test", "testpass");
+            smtpTransport.issueCommand("AUTH LOGIN ", 334);
+            assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 VXNlciBOYW1lAA==" /* Username */));
+            smtpTransport.issueCommand(Base64.getEncoder().encodeToString("test".getBytes(StandardCharsets.US_ASCII)), -1);
+            assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace("334 UGFzc3dvcmQA" /* Password */));
+            smtpTransport.issueCommand(Base64.getEncoder().encodeToString("testpass".getBytes(StandardCharsets.US_ASCII)), -1);
+            assertThat(smtpTransport.getLastServerResponse(), equalToCompressingWhiteSpace(AuthCommand.AUTH_SUCCEDED));
+        } finally {
+            smtpTransport.close();
+        }
+    }
+
+    @Test
+    public void mailSenderAUTHSuffix() throws IOException, MessagingException {
+        Session smtpSession = greenMail.getSmtp().createSession();
         SMTPTransport smtpTransport = new SMTPTransport(smtpSession, smtpURL);
 
         try {
-            smtpSocket = new Socket(hostAddress, port);
+            Socket smtpSocket = new Socket(hostAddress, port); // Closed by transport
             smtpTransport.connect(smtpSocket);
             assertThat(smtpTransport.isConnected(), is(equalTo(true)));
             smtpTransport.issueCommand("MAIL FROM: <test.test@test.net> AUTH <>", -1);
