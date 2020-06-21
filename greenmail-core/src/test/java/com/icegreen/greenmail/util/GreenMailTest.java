c962dc1d7fb25fc1769303542fdabf09fa65ea8f
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-06-21 13:28:49 +0200

    String.contains is case sensitive so both participants needs to be lowercase

--- greenmail-core/src/test/java/com/icegreen/greenmail/util/GreenMailTest.java
+++ greenmail-core/src/test/java/com/icegreen/greenmail/util/GreenMailTest.java
@@ -4,6 +4,8 @@ import com.icegreen.greenmail.junit.GreenMailRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import javax.mail.internet.MimeMessage;
+
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
@@ -28,6 +30,24 @@ public class GreenMailTest {
         assertThat(finish - start, is(lessThan(timePasswdMax)));
         final long timePassedMin = (long) (timeout * 0.9f);
         assertThat(finish - start, is(greaterThan(timePassedMin)));
+    }
+
+    @Test
+    public void getReceivedMessagesForDomainLowerCaseRecipientAddress() {
+        final String to = "to@localhost.com";
+        GreenMailUtil.sendTextEmailTest(to, "from@localhost.com", "subject", "body");
+
+        final MimeMessage[] receivedMessagesForDomain = greenMail.getReceivedMessagesForDomain(to);
+        assertThat(receivedMessagesForDomain.length, is(1));
+    }
+
+    @Test
+    public void getReceivedMessagesForDomainWithUpperCaseRecipientAddress() {
+        final String to = "someReceiver@localhost.com";
+        GreenMailUtil.sendTextEmailTest(to, "from@localhost.com", "subject", "body");
+
+        final MimeMessage[] receivedMessagesForDomain = greenMail.getReceivedMessagesForDomain(to);
+        assertThat(receivedMessagesForDomain.length, is(1));
     }
 }
 
