26c2e38043b8bb260c91568adf26c2b7b1d54914
Marcel May <marcel.may.de@gmail.com>
2021-04-07 23:14:10 +0200

    Tuning long running test, removing TIME_WAIT workaround

--- greenmail-core/src/test/java/com/icegreen/greenmail/test/ConcurrentCloseIT.java
+++ greenmail-core/src/test/java/com/icegreen/greenmail/test/ConcurrentCloseIT.java
@@ -1,6 +1,5 @@
 package com.icegreen.greenmail.test;
 
-import java.util.concurrent.TimeUnit;
 import javax.mail.internet.MimeMessage;
 
 import com.icegreen.greenmail.util.GreenMail;
@@ -16,8 +15,6 @@ public class ConcurrentCloseIT {
     public void concurrentCloseTest() throws Exception {
         for (int i = 0; i < 5000; i++) {
             testThis();
-            // Avoid TIME_WAIT issues.
-            TimeUnit.MILLISECONDS.sleep(2);
         }
     }
 
