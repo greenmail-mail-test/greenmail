ef66e428ac0c35dcd4cc90023f54815875f35e38
Marcel May <marcel.may.de@gmail.com>
2022-11-06 23:12:59 +0100

    CI: Workaround for slow ci instances causing context-deadline-exceeded (backport)
    Reduce iterations and log progress

--- greenmail-core/src/test/java/com/icegreen/greenmail/test/ConcurrentCloseIT.java
+++ greenmail-core/src/test/java/com/icegreen/greenmail/test/ConcurrentCloseIT.java
@@ -6,16 +6,25 @@ import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConcurrentCloseIT {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Test
     public void concurrentCloseTest() throws Exception {
-        for (int i = 0; i < 5000; i++) {
+        final int limit = 3000;
+        final long startTime = System.currentTimeMillis();
+        for (int i = 0; i < limit; i++) {
             testThis();
+            if (i > 0 && i % 500 == 0) {
+                log.info("Performed {} of {} iterations in {}ms", i, limit, System.currentTimeMillis() - startTime);
+            }
         }
+        log.info("Performed {} iterations in {}ms", limit, System.currentTimeMillis() - startTime);
     }
 
     private void testThis() throws InterruptedException {
@@ -43,7 +52,8 @@ public class ConcurrentCloseIT {
 
         public void run() {
             try {
-                GreenMailUtil.sendTextEmail("test@localhost", "from@localhost", "abc", "def", ServerSetupTest.SMTP);
+                GreenMailUtil.sendTextEmail("test@localhost", "from@localhost", "abc", "def",
+                    ServerSetupTest.SMTP);
             } catch (final Throwable e) {
                 exc = new IllegalStateException(e);
             }
