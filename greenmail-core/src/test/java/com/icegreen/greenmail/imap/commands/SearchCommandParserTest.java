e7250f52f6a0bb6e28d6f62a92c3bbc67aaaa369
Marcel May <marcel.may.de@gmail.com>
2023-09-14 22:54:07 +0200

    Backport: SearchCommandParser$SearchOperator cannot be cast to j.m.search.SearchTerm (fixes #638)
    Backport of #591

--- greenmail-core/src/test/java/com/icegreen/greenmail/imap/commands/SearchCommandParserTest.java
+++ greenmail-core/src/test/java/com/icegreen/greenmail/imap/commands/SearchCommandParserTest.java
@@ -11,17 +11,19 @@ import com.icegreen.greenmail.imap.ImapRequestLineReader;
 import com.icegreen.greenmail.imap.ProtocolException;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchCommandParserTest {
     @Test
     public void testHeader() throws ProtocolException {
         SearchTerm expectedTerm = new AndTerm(
-                new AndTerm(new SearchTerm[]{
-                        new HeaderTerm("Message-ID", "<1627010197.0.1593681191102@[192.168.242.10]>"),
-                        new FlagTerm(new Flags(Flags.Flag.SEEN), true)
-                }),
-                SearchTermBuilder.create(SearchKey.ALL).build()
+            new AndTerm(new SearchTerm[]{
+                new HeaderTerm("Message-ID", "<1627010197.0.1593681191102@[192.168.242.10]>"),
+                new FlagTerm(new Flags(Flags.Flag.SEEN), true)
+            }),
+            SearchTermBuilder.create(SearchKey.ALL).build()
         );
 
         SearchTerm searchTerm = parse("(HEADER Message-ID <1627010197.0.1593681191102@[192.168.242.10]> SEEN) ALL");
@@ -56,12 +58,12 @@ public class SearchCommandParserTest {
     @Test
     public void testAndSubjectOrToFrom() throws ProtocolException, AddressException {
         SearchTerm expectedTerm = new AndTerm(new SearchTerm[]{
-                new SubjectTerm("Greenmail"),
-                new OrTerm(
-                        new RecipientTerm(Message.RecipientType.TO, new InternetAddress("to@localhost")),
-                        new FromTerm(new InternetAddress("from@localhost"))
-                ),
-                SearchTermBuilder.create(SearchKey.ALL).build()});
+            new SubjectTerm("Greenmail"),
+            new OrTerm(
+                new RecipientTerm(Message.RecipientType.TO, new InternetAddress("to@localhost")),
+                new FromTerm(new InternetAddress("from@localhost"))
+            ),
+            SearchTermBuilder.create(SearchKey.ALL).build()});
 
         // SUBJECT Greenmail (OR TO from@localhost FROM from@localhost) ALL
         SearchTerm searchTerm = parse("SUBJECT Greenmail OR TO to@localhost FROM from@localhost ALL");
@@ -73,8 +75,44 @@ public class SearchCommandParserTest {
     public void testNotKeyword() throws ProtocolException {
         Flags flags = new Flags();
         flags.add("ABC");
-		SearchTerm expectedTerm = new NotTerm(new FlagTerm(flags, true));
+        SearchTerm expectedTerm = new NotTerm(new FlagTerm(flags, true));
         SearchTerm searchTerm = parse("NOT (KEYWORD ABC)");
+
+        assertThat(searchTerm).isEqualTo(expectedTerm);
+    }
+
+    @Test
+    public void testSimpleOr() throws ProtocolException {
+        SearchTerm expectedTerm = new OrTerm(
+            new FlagTerm(new Flags(Flags.Flag.DRAFT), true),
+            new FlagTerm(new Flags(Flags.Flag.SEEN), true)
+        );
+        SearchTerm searchTerm = parse("OR (DRAFT) (SEEN)");
+
+        assertThat(searchTerm).isEqualTo(expectedTerm);
+    }
+
+    @Test
+    public void testIssue591simple() throws ProtocolException {
+        SearchTerm expectedTerm = new OrTerm(
+            new NotTerm(new FlagTerm(new Flags(Flags.Flag.SEEN), true)),
+            new FlagTerm(new Flags(Flags.Flag.SEEN), true)
+        );
+        SearchTerm searchTerm = parse("OR (NOT (SEEN)) (SEEN)");
+
+        assertThat(searchTerm).isEqualTo(expectedTerm);
+    }
+
+    @Test
+    public void testIssue591complex() throws ProtocolException {
+        SearchTerm expectedTerm = new OrTerm(
+            new NotTerm(new FlagTerm(new Flags(Flags.Flag.SEEN), true)),
+            new OrTerm(
+                new FlagTerm(new Flags("foo"), true),
+                new FlagTerm(new Flags("bar"), true)
+            )
+        );
+        SearchTerm searchTerm = parse("OR (NOT (SEEN)) (OR (KEYWORD foo) (KEYWORD bar))");
 
         assertThat(searchTerm).isEqualTo(expectedTerm);
     }
