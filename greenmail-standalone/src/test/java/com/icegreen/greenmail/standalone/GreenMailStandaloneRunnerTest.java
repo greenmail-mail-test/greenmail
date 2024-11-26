bc11fd12143124ad5e5c32a210fe5cc97691c0ce
Shlomi <shlomih@lightrun.com>
2024-11-26 18:29:15 +0200

    fix handling of `greenmail.sieve.ignore.detail`

--- greenmail-standalone/src/test/java/com/icegreen/greenmail/standalone/GreenMailStandaloneRunnerTest.java
+++ greenmail-standalone/src/test/java/com/icegreen/greenmail/standalone/GreenMailStandaloneRunnerTest.java
@@ -81,7 +81,7 @@ public class GreenMailStandaloneRunnerTest {
             "{\"port\":3143,\"address\":\"127.0.0.1\",\"protocol\":\"imap\",\"isSecure\":false,\"readTimeout\":-1," +
             "\"writeTimeout\":-1,\"connectionTimeout\":-1,\"serverStartupTimeout\":2000,\"isDynamicPort\":false}]," +
             "\"authenticationDisabled\":false," +
-            "\"sieveIgnoreDetail\":true," +
+            "\"sieveIgnoreDetail\":false," +
             "\"preloadDirectory\":null" +
             "}");
 
