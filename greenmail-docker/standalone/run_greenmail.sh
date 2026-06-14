9a87a8e99ae77b9cd21c97311e4a2be042bc3cfb
Marcel May <marcel.may.de@gmail.com>
2026-06-14 12:12:04 +0200

    Improve Dockerfile and fix shebang (fixes #1005)

--- greenmail-docker/standalone/run_greenmail.sh
+++ greenmail-docker/standalone/run_greenmail.sh
@@ -1,3 +1,3 @@
-#/bin/sh
+#!/bin/sh
 echo "Executing 'java $JAVA_OPTS $GREENMAIL_OPTS $GREENMAIL_ADDITIONAL_OPTS -jar greenmail-standalone.jar' ..."
 exec java $JAVA_OPTS $GREENMAIL_OPTS $GREENMAIL_ADDITIONAL_OPTS -jar greenmail-standalone.jar
